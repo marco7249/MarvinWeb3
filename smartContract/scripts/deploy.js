e2bff0a78c01d0bef9b79dbfa002b873e813b78c
marvin <x6688w36@gmail.com>
2022-04-01 03:09:25 +0800

    fix deploy.js problem

--- smartContract/scripts/deploy.js
+++ smartContract/scripts/deploy.js
@@ -1,7 +1,7 @@
 const main = async () => {
 
   const Transactions = await hre.ethers.getContractFactory("Transactions");
-  const transactions = await Greeter.deploy();
+  const transactions = await Transactions.deploy();
 
   await transactions.deployed();
 

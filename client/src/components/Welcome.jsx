474ef023c96740541e4f4b470d8810ea1db77e73
marvin <x6688w36@gmail.com>
2022-04-08 11:07:03 +0800

    complete trasaction on blockchain

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,30 +1,35 @@
+import React, { useContext } from 'react';
 import { AiFillPayCircle } from 'react-icons/ai';
-import {SiEthereum } from 'react-icons/si';
-import {BsInfoCircle} from 'react-icons/bs';
+import { SiEthereum } from 'react-icons/si';
+import { BsInfoCircle } from 'react-icons/bs';
 
-import {Loader} from './';
+import { TransactionContext } from '../context/TransactionContext';
+import { Loader } from './';
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input = ({placeholder, name , type, value , handleChange}) => (
+const Input = ({ placeholder, name, type, value, handleChange }) => (
     <input
-    placeholder={placeholder}
-    type={type}
-    step="0.0001"
-    value={value}
-    onChange={(e) => handleChange(e, name)}
-    className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+        placeholder={placeholder}
+        type={type}
+        step="0.0001"
+        value={value}
+        onChange={(event) => handleChange(event, name)}
+        className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
     />
-  );
+);
 
 const Welcome = () => {
+    const { connectWallet, currentAccount, formData, sendTransaction, handleChange, isLoading } = useContext(TransactionContext);
 
-    const connectWallet = () => {
+    const handleSubmit = (event) => {
+        const {addressTo, amount, keyword, message} = formData;
 
-    }
+        event.preventDefault();
 
-    const handleSubmit = () => {
+        if(!addressTo || !amount || !keyword || !message ) return;
 
+        sendTransaction();
     }
 
     return (
@@ -32,14 +37,19 @@ const Welcome = () => {
             <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
                 <div className='flex flex-1 justify-start flex-col mf:mr-10'>
                     <h1 className='text-3xl sm:text-5xl text-white text-gradient'>
-                        Send Crypto <br /> across the world 
+                        Send Crypto <br /> across the world
                     </h1>
                     <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
                         Explore the crypto world. Buy and sell cryptocurrencies easily on here.
                     </p>
-                    <button type='button' onClick={connectWallet} className = 'flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'>
-                        <p className='text-white text-base font-semibold'>Connect Wallet</p>
-                    </button>
+
+                    {
+                        !currentAccount &&
+                        <button type='button' onClick={connectWallet} className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'>
+                            <p className='text-white text-base font-semibold'>Connect Wallet</p>
+                        </button>
+
+                    }
 
                     <div className='grid grid-cols-3 w-full mt-10'>
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
@@ -58,9 +68,9 @@ const Welcome = () => {
                         <div className='flex justify-between flex-col w-full h-full'>
                             <div className='flex justify-between items-start'>
                                 <div className='w-10 h-10 rounded-full border-2 border-white flex justify-center items-center'>
-                                    <SiEthereum fontSize={21} color='#fff'/>
+                                    <SiEthereum fontSize={21} color='#fff' />
                                 </div>
-                                <BsInfoCircle fontSize={17} color='#fff'/>
+                                <BsInfoCircle fontSize={17} color='#fff' />
                             </div>
                             <div>
                                 <p className='text-white font-light text-sm'>
@@ -74,27 +84,27 @@ const Welcome = () => {
                     </div>
 
                     <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
-                        <Input placeholder='Address To' name='addressTo' type='text' handleChange={() => {}}/>
-                        <Input placeholder='Amount (ETH)' name='amount' type='number' handleChange={() => {}}/>
-                        <Input placeholder='Keyword (GIF)' name='keyword' type='text' handleChange={() => {}}/>
-                        <Input placeholder='Enter Message' name='message' type='text' handleChange={() => {}}/>
-                        
+                        <Input placeholder='Address To' name='addressTo' type='text' handleChange={handleChange} />
+                        <Input placeholder='Amount (ETH)' name='amount' type='number' handleChange={handleChange} />
+                        <Input placeholder='Keyword (GIF)' name='keyword' type='text' handleChange={handleChange} />
+                        <Input placeholder='Enter Message' name='message' type='text' handleChange={handleChange} />
+
                         <div className='h-[1px] w-full bg-gray-400 my-2' />
-                        
+
                         {
-                            false ? (
-                            <Loader />
-                            ) 
-                            : 
-                            (
-                                <button 
-                                type='button' 
-                                onClick= {handleSubmit}
-                                className='rounded-full text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] cursor-pointer'
-                                >
-                                Send now
-                                </button>
+                            isLoading ? (
+                                <Loader />
                             )
+                                :
+                                (
+                                    <button
+                                        type='button'
+                                        onClick={handleSubmit}
+                                        className='rounded-full text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] cursor-pointer'
+                                    >
+                                        Send now
+                                    </button>
+                                )
                         }
                     </div>
                 </div>
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transaction.json';
+
+export const contractABI  = abi.abi;
+export const contractAddress = '0x2CcB1531c2814f701a0096F627801DcfAD349a56';
